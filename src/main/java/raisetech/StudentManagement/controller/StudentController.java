b605a79ec426bc6e70307e18359579d5274203ae
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-03-15 23:29:43 +0900

    講義動画２８の課題手前まで完了

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
@@ -34,42 +34,6 @@ public class StudentController {
     model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
     return "studentList";
   }
-  //下のstudents.forEach( student -> {  から　return studentDetails;  まではすぐ下の/* から */ までと同じこと！！
-    /*for (Student student : students) {
-      StudentDetail studentDetail = new StudentDetail();
-      studentDetail.setStudent(student);
-
-      List<StudentsCourses> convertStudentCourses = new ArrayList<>();
-
-      for (StudentsCourses studentCourse : studentsCourses) {
-        if (student.getId().equals(studentCourse.getStudentId())) {
-          convertStudentCourses.add(studentCourse);
-        }
-      }
-
-      studentDetail.setStudentsCourses(convertStudentCourses);
-      studentDetails.add(studentDetail);
-    }
-
-    return studentDetails;*/
-
-
-  /*private List<StudentDetail> convertStudentDetails(List<Student> students,List<StudentsCourses> studentsCourses) {
-    List<StudentDetail> studentDetails = new ArrayList<>();
-
-    students.forEach(student -> {
-      StudentDetail studentDetail = new StudentDetail();
-      studentDetail.setStudent(student);
-
-      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
-          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
-          .collect(Collectors.toList());
-
-      studentDetail.setStudentsCourses(convertStudentCourses);
-      studentDetails.add(studentDetail);
-    });
-    return studentDetails;
-  }*/
 
   @GetMapping("/studentsCourseList")
   public List<StudentsCourses> getStudentsCourseList() {
@@ -83,7 +47,7 @@ public class StudentController {
     return "registerStudent";
   }
 
-  @PatchMapping("/registerStudent")
+  @PostMapping("/registerStudent")
   public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
     if (result.hasErrors()) {
       return "registerStudent";
