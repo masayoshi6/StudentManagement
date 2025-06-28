41c1746e09f41b1874eca171c1e49d04ebce6256
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-06-28 16:00:51 +0900

    講義動画４３の課題作成

--- src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
@@ -43,17 +43,7 @@ class StudentRepositoryTest {
 
   @Test
   void 受講生の単体検索ができること() {
-    Student student = new Student();
-    student.setId("1");
-    student.setName("山田太郎");
-    student.setKanaName("ヤマダタロウ");
-    student.setNickname("タロ");
-    student.setEmail("taro@example.com");
-    student.setArea("東京");
-    student.setAge(25);
-    student.setSex("男性");
-    student.setRemark("");
-    student.setDeleted(false);
+    Student student = getStudent();
 
     Student actual = sut.searchStudent("1");
     assertEquals(student.getId(), actual.getId());
@@ -66,6 +56,7 @@ class StudentRepositoryTest {
     assertEquals(student.getSex(), actual.getSex());
   }
 
+
   @Test
   void 受講生のコース情報の全件検索が行えること() {
     List<StudentCourse> actual = sut.searchStudentCourseList();
@@ -76,44 +67,29 @@ class StudentRepositoryTest {
   void 受講生IDに紐づく受講生コース情報の検索ができること() {
     Student student = new Student();
     student.setId("1");
-    student.setName("山田太郎");
-    student.setKanaName("ヤマダタロウ");
-    student.setNickname("タロ");
-    student.setEmail("taro@example.com");
-    student.setArea("東京");
-    student.setAge(25);
-    student.setSex("男性");
-    student.setRemark("");
-    student.setDeleted(false);
 
-    StudentCourse studentCourse1 = new StudentCourse();
-    studentCourse1.setId("1");
-    studentCourse1.setStudentId("1");
-    studentCourse1.setCourseName("Javaコース");
-    studentCourse1.setCourseStartAt(LocalDateTime.of(2023, 4, 1, 9, 0, 0));
-    studentCourse1.setCourseEndAt(LocalDateTime.of(2023, 7, 1, 15, 0, 0));
+    StudentCourse studentCourse1 = getStudentCourse("1", "Javaコース");
 
-    StudentCourse studentCourse2 = new StudentCourse();
-    studentCourse2.setId("2");
-    studentCourse2.setStudentId("1");
-    studentCourse2.setCourseName("AWSコース");
-    studentCourse2.setCourseStartAt(LocalDateTime.of(2023, 5, 1, 10, 0, 0));
-    studentCourse2.setCourseEndAt(LocalDateTime.of(2023, 8, 1, 16, 0, 0));
+    StudentCourse studentCourse2 = getStudentCourse("2", "AWSコース");
 
-    StudentCourse studentCourse3 = new StudentCourse();
-    studentCourse3.setId("3");
-    studentCourse3.setStudentId("1");
-    studentCourse3.setCourseName("Web制作コース");
-    studentCourse3.setCourseStartAt(LocalDateTime.of(2024, 1, 1, 13, 0, 0));
-    studentCourse3.setCourseEndAt(LocalDateTime.of(2024, 5, 1, 19, 0, 0));
+    StudentCourse studentCourse3 = getStudentCourse("10", "Web制作コース");
 
     List<StudentCourse> actual = sut.searchStudentCourse("1");
     assertEquals(student.getId(), studentCourse1.getStudentId());
+    assertEquals(student.getId(), studentCourse2.getStudentId());
     assertEquals(student.getId(), studentCourse3.getStudentId());
     List<String> courseNames = List.of(studentCourse1.getCourseName(),
         studentCourse2.getCourseName(), studentCourse3.getCourseName());
     assertEquals(courseNames.size(), actual.size());
     assertThat(actual.size()).isEqualTo(3);
+  }
+
+  private StudentCourse getStudentCourse(String number, String courseName) {
+    StudentCourse studentCourse1 = new StudentCourse();
+    studentCourse1.setId(number);
+    studentCourse1.setStudentId("1");
+    studentCourse1.setCourseName(courseName);
+    return studentCourse1;
   }
 
   @Test
@@ -132,6 +108,8 @@ class StudentRepositoryTest {
 
   @Test
   void 受講生更新ができること() {
+
+    //ためしにメールアドレス、住んでいる地域、年齢の３つの更新テストを実行してみます。
     Student student = new Student();
     student.setId("1");
     student.setName("山田太郎");
@@ -152,4 +130,34 @@ class StudentRepositoryTest {
     assertEquals(student.getAge(), actual.getAge());
   }
 
+  @Test
+  void 受講生コース情報のコース名が更新できること() {
+    Student student = getStudent();
+
+    StudentCourse studentCourse = getStudentCourse("1", "数学コース");
+    studentCourse.setCourseStartAt(LocalDateTime.of(2023, 4, 1, 9, 0, 0));
+    studentCourse.setCourseEndAt(LocalDateTime.of(2023, 7, 1, 15, 0, 0));
+
+    sut.updateStudentCourse(studentCourse);
+
+    assertEquals(student.getId(), studentCourse.getStudentId());
+
+    assertEquals(studentCourse.getCourseName(), "数学コース");
+
+  }
+
+  private Student getStudent() {
+    Student student = new Student();
+    student.setId("1");
+    student.setName("山田太郎");
+    student.setKanaName("ヤマダタロウ");
+    student.setNickname("タロ");
+    student.setEmail("taro@example.com");
+    student.setArea("東京");
+    student.setAge(25);
+    student.setSex("男性");
+    student.setRemark("");
+    student.setDeleted(false);
+    return student;
+  }
 }
